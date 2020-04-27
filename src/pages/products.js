13783f606fcd49b9201739653995170df1ab234f
Leonardo Miranda <leonardomisi@hotmail.com>
2020-04-27 12:20:34 -0300

    SolucionandoBugs

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {WebView} from 'react-native-webview';
-//import {WebView} from 'react-native';
 
 const Product = ({navigation}) => (
   <WebView source={{uri: navigation.state.params.product.url}} />
